a0afd710f799a2a72be6e6ce5979a0c7b60907b7
Nikki Strømsnes <nikki.stromsnes@akqa.com>
2018-11-26 09:30:43 +0100

    Add simple error handling for initial server fetch

--- src/server/index.js
+++ src/server/index.js
@@ -16,32 +16,36 @@ server.get('*', (req, res) => {
   let city = req.query.city || 'Copenhagen'
 
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},dk&appid=166d00e26d3ff2c6149e89feccc5c59a`)
-    .then(res => res.json)
+    .then(res => res.json())
     .then(json => {
-      const props = {
-        city: json.name,
-        temperature: json.main.temp,
-        humidity: json.main.humidity,
-        windSpeed: json.wind.speed,
-        windAngle: json.wind.angle,
+      try {
+        const props = {
+          city: json.name,
+          temperature: json.main.temp,
+          humidity: json.main.humidity,
+          windSpeed: json.wind.speed,
+          windAngle: json.wind.angle,
+        }
+
+        const body = renderToString(<App
+          city={city}
+          temperature={json.main.temp}
+          humidity={json.main.humidity}
+          windSpeed={json.wind.speed}
+          windAngle={json.wind.deg}
+        />)
+        const title = `🌤 Weather in ${city}`
+
+        res.send(
+          Html({
+            body,
+            title,
+            props,
+          })
+        )
+      } catch (ex) {
+        throw new Error(ex)
       }
-
-      const body = renderToString(<App
-        city={city}
-        temp={json.main.temp}
-        humidity={json.main.humidity}
-        windSpeed={json.wind.speed}
-        windAngle={json.wind.deg}
-      />)
-      const title = `🌤 Weather in ${city}`
-
-      res.send(
-        Html({
-          body,
-          title,
-          props,
-        })
-      )
   }).catch(function(ex) {
     console.log('parsing failed', ex)
   })
